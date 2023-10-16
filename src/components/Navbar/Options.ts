943d61be8803f5d1774f9092774f34b635989619
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-16 07:25:35 -0500

    feat(ui): Create responsive and reactive navbar

---
+++ src/components/Navbar/Options.ts
@@ -0,0 +1,26 @@
+export const NavigationOptions = {
+  user: [
+    {
+      name: "Files",
+      to: "/files"
+    },
+    {
+      name: "Profile",
+      to: "/profile"
+    },
+    {
+      name: "Logout",
+      to: "/logout"
+    }
+  ],
+  default: [
+    {
+      name: "Login",
+      to: "/login"
+    },
+    {
+      name: "Register",
+      to: "/register"
+    }
+  ]
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes } from "react-router-dom";
+import { Toaster } from "sonner";
 
+import { Navbar } from "./components/Navbar/Navbar";
+import { AuthContextProvider } from "./context/AuthContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes></Routes>
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Toaster expand closeButton richColors />
+        <Routes></Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   </React.StrictMode>
 );
