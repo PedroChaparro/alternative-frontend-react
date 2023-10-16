02ab307c152aa633016743ee78160476c35d5256
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-16 10:35:11 -0500

    feat: Add service to get user files

---
+++ src/services/files/list-files.service.ts
@@ -0,0 +1,51 @@
+import { ENVIRONMENT } from "@/config/environment";
+import { File } from "@/types/entities";
+import axios, { AxiosError } from "axios";
+
+type ListFilesRequest = {
+  token: string;
+  directory?: string;
+};
+
+type ListFilesResponse = {
+  success: boolean;
+  msg: string;
+  files: File[];
+};
+
+export const listFilesService = async (
+  req: ListFilesRequest
+): Promise<ListFilesResponse> => {
+  const URL = req.directory
+    ? `${ENVIRONMENT.PROXY_BASE_URL}/file/list?directory=${req.directory}`
+    : `${ENVIRONMENT.PROXY_BASE_URL}/file/list`;
+
+  try {
+    const listFilesResponse = await axios.get(URL, {
+      headers: {
+        Authorization: `Bearer ${req.token}`
+      }
+    });
+    const { data } = listFilesResponse;
+    console.log(data);
+
+    return {
+      success: true,
+      msg: data.msg,
+      files: data.files
+    };
+  } catch (error) {
+    let errorMsg = "There was an error while trying to list files";
+    console.log(error);
+
+    if (error instanceof AxiosError) {
+      errorMsg = error.response?.data.msg || errorMsg;
+    }
+
+    return {
+      success: false,
+      msg: errorMsg,
+      files: []
+    };
+  }
+};
---
+++ src/types/entities.ts
@@ -0,0 +1,7 @@
+export type File = {
+  name: string;
+  isFile: boolean;
+  uuid: string;
+  size: number;
+  extension?: string;
+};
