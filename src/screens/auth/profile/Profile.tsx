0896c6f4ca79553211d13b6626bf505e3a52baea
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-16 19:27:12 -0500

    fix(ui): Improve profile page responsive

--- src/screens/auth/profile/Profile.tsx
+++ src/screens/auth/profile/Profile.tsx
@@ -9,19 +9,23 @@ export const ProfilePage = () => {
   const initial = session?.username[0].toUpperCase();
 
   return (
-    <main className="mx-auto mt-4 flex max-w-md flex-col items-center gap-4 border p-4">
-      <div className="grid h-16 w-16 place-content-center rounded-sm bg-primary text-white">
-        <span className="text-lg font-bold">{initial}</span>
-      </div>
-      <h1 className="text-xl font-medium tracking-wide">{session?.username}</h1>
-      <Tabs defaultValue="update-password" className="w-full">
-        <TabsList>
-          <TabsTrigger value="update-password">Update password</TabsTrigger>
-        </TabsList>
-        <TabsContent value="update-password">
-          <UpdatePasswordForm />
-        </TabsContent>
-      </Tabs>
+    <main className="mt-4 p-4">
+      <section className="mx-auto flex max-w-md flex-col items-center gap-4 border p-4">
+        <div className="grid h-16 w-16 place-content-center rounded-sm bg-primary text-white">
+          <span className="text-lg font-bold">{initial}</span>
+        </div>
+        <h1 className="text-xl font-medium tracking-wide">
+          {session?.username}
+        </h1>
+        <Tabs defaultValue="update-password" className="w-full">
+          <TabsList>
+            <TabsTrigger value="update-password">Update password</TabsTrigger>
+          </TabsList>
+          <TabsContent value="update-password">
+            <UpdatePasswordForm />
+          </TabsContent>
+        </Tabs>
+      </section>
     </main>
   );
 };
