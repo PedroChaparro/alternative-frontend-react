92e8eb607e729789222515ceaa34ab59cf8c5d89
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-17 16:55:53 -0500

    feat: Create dialog and form to rename a file

---
+++ src/screens/files/dialogs/rename-file/RenameFileDialog.tsx
@@ -0,0 +1,37 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog";
+import { FilesDialogsContext } from "@/context/FilesDialogsContext";
+import { Dialogs } from "@/types/enums";
+import { useContext } from "react";
+
+import { RenameFileForm } from "./RenameFileForm";
+
+export const RenameFileDialog = () => {
+  const { dialogsOpenState, updateDialogOpenState } =
+    useContext(FilesDialogsContext);
+
+  return (
+    <Dialog
+      open={dialogsOpenState.RENAME_FILE}
+      onOpenChange={(open: boolean) => {
+        updateDialogOpenState(Dialogs.RENAME_FILE, open);
+      }}
+    >
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Rename file</DialogTitle>
+          <DialogDescription>
+            Update the name of the file. Click on the rename button when you're
+            done.
+          </DialogDescription>
+        </DialogHeader>
+        <RenameFileForm />
+      </DialogContent>
+    </Dialog>
+  );
+};
