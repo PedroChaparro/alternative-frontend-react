82bec4c43bf8d59c3180ae408e754ba06c5d1dbf
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-10-21 13:26:36 -0500

    fix: service

--- src/services/files/download-file.service.ts
+++ src/services/files/download-file.service.ts
@@ -1,7 +1,6 @@
 import { ENVIRONMENT } from "@/config/environment";
 import axios, { AxiosError } from "axios";
 
-
 type DownloadFileRequest = {
   token: string;
   fileUUID: string;
@@ -10,40 +9,24 @@ type DownloadFileRequest = {
 type DownloadFileResponse = {
   success: boolean;
   msg: string;
-  fileContent?: Blob;
-  fileName?: string;
 };
 
 export const downloadFileService = async (
   req: DownloadFileRequest
 ): Promise<DownloadFileResponse> => {
   try {
-    const downloadFileResponse = await axios.get(
+    await axios.get(
       `${ENVIRONMENT.PROXY_BASE_URL}/file/download/${req.fileUUID}`,
       {
         headers: {
           Authorization: `Bearer ${req.token}`
-        },
-        responseType: "blob" // Set the response type to "blob" to handle binary data
+        }
       }
     );
-    const { data, headers } = downloadFileResponse;
-
-    // Extract the filename from the response headers (if available)
-    const contentDisposition = headers["content-disposition"];
-    let fileName = "";
-    if (contentDisposition) {
-      const matches = contentDisposition.match(/filename="(.+)"/);
-      if (matches) {
-        fileName = matches[1];
-      }
-    }
-
+    
     return {
       success: true,
-      msg: "File downloaded successfully",
-      fileContent: data,
-      fileName: fileName
+      msg: "File downloaded successfully"
     };
   } catch (error) {
     let errorMsg = "There was an error while trying to download the file";
