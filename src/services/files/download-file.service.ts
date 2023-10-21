a491244e3ab050a0b39b1eef8a91cc22c172954c
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-10-21 14:18:14 -0500

    refactor: Remove try catch block from the download service

--- src/services/files/download-file.service.ts
+++ src/services/files/download-file.service.ts
@@ -1,5 +1,5 @@
 import { ENVIRONMENT } from "@/config/environment";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 type DownloadFileRequest = {
   token: string;
@@ -9,25 +9,15 @@ type DownloadFileRequest = {
 export const downloadFileService = async (
   req: DownloadFileRequest
 ): Promise<Blob> => {
-  try {
-    const response = await axios.get(
-      `${ENVIRONMENT.PROXY_BASE_URL}/file/download/${req.fileUUID}`,
-      {
-        headers: {
-          Authorization: `Bearer ${req.token}`
-        },
-        responseType: "blob"
-      }
-    );
-
-    return response.data;
-  } catch (error) {
-    let errorMsg = "There was an error while trying to download the file";
-
-    if (error instanceof AxiosError) {
-      errorMsg = error.response?.data.msg || errorMsg;
+  const response = await axios.get(
+    `${ENVIRONMENT.PROXY_BASE_URL}/file/download/${req.fileUUID}`,
+    {
+      headers: {
+        Authorization: `Bearer ${req.token}`
+      },
+      responseType: "blob"
     }
+  );
 
-    throw new Error(errorMsg);
-  }
+  return response.data;
 };
