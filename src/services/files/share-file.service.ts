41680be8bb7cbf4a264cbf3bca32dc921999fa6c
Miguel Mateo Mendoza Rojas <115849391+MiguelMRojas@users.noreply.github.com>
2023-10-22 15:57:57 -0500

    feat: service file share

---
+++ src/services/files/share-file.service.ts
@@ -0,0 +1,51 @@
+import axios, { AxiosError } from "axios";
+import { ENVIRONMENT } from "@/config/environment";
+
+type ShareFileRequest = {
+  token: string;
+  fileUUID: string;
+  otherUsername: string;
+};
+
+type ShareFileResponse = {
+  success: boolean;
+  msg: string;
+};
+
+export const shareFileService = async (
+  req: ShareFileRequest
+): Promise<ShareFileResponse> => {
+  const URL = `${ENVIRONMENT.PROXY_BASE_URL}/file/share`;
+
+  try {
+    const shareFileResponse = await axios.post(
+      URL,
+      {
+        fileUUID: req.fileUUID,
+        otherUsername: req.otherUsername
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${req.token}`
+        }
+      }
+    );
+    const { data } = shareFileResponse;
+
+    return {
+      success: true,
+      msg: data.msg
+    };
+  } catch (error) {
+    let errorMsg = "There was an error while trying to share the file";
+
+    if (error instanceof AxiosError) {
+      errorMsg = error.response?.data.msg || errorMsg;
+    }
+
+    return {
+      success: false,
+      msg: errorMsg
+    };
+  }
+};
